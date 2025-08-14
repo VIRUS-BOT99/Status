8888b8de035edf24e8e0b15fe0d0ebe755c8251c
VIRUS-BOT99 <iimmeesshhbuddimal2009@gmail.com>
2025-08-14 21:46:01 +0530

    Add files via upload

---
+++ config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    AUTO_RECORDING: true, 
+    triggerWords: [
+        'send',
+        'dpn',
+        '보내다'
+    ],
+
+    OWNER_NUMBERS: [
+        '94789958225', 
+        '94767799548'  
+    ],
+
+    SESSION_PATH: './session'
+};
